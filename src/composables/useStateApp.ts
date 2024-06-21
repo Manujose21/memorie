9b18e3bd1cd6c491c397403a8076ec01149f718d
manujose21 <m4nufra@gmail.com>
2024-06-21 17:54:57 -0400

    inital menu

--- src/composables/useStateApp.ts
+++ src/composables/useStateApp.ts
@@ -1,7 +1,7 @@
 import type { StatusApp } from '@/types'
 import { ref } from 'vue'
 
-const STATE_APP = ref<StatusApp>('GAME')
+const STATE_APP = ref<StatusApp>('MENU')
 export const useStateApp = () => {
   return {
     STATE_APP
